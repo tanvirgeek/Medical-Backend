8fdbf0cdcbc833a78bfec45676b54c76aa484de0
tanvirgeek <tanvirgeek@gmail.comm>
2020-02-02 14:38:09 +0600

    The register backened PHP

---
+++ init.php
@@ -0,0 +1,11 @@
+<?php
+    $host = "localhost";
+    $username = "root";
+    $password = "tanvirscc124867";
+    $db_name = "medical_quiz";
+
+    $con = mysqli_connect($host, $username,$password,$db_name);
+
+   
+
+?>
---
+++ register.php
@@ -0,0 +1,34 @@
+<?php
+
+    require "init.php";
+    $user_name = $_GET["username"];
+    $password = $_GET["password"];
+    $email = $_GET["email"];
+    $fullname = $_GET["fullname"];
+    $college = $_GET["college"];
+    $gender = $_GET["gender"];
+    $dob = $_GET["DOB"];
+
+    //echo $user_name;
+
+    $sql = "select * from users where Username = '$user_name'";
+
+    $result = mysqli_query($con,$sql);
+    // checking if username exists or not
+    if (mysqli_num_rows($result)>0){
+        $status = "exists";
+        echo "The username exists";
+    }else{
+        $sql = "insert into users (Username, Password, Email, Fullname, College, Gender, DOB) values('$user_name','$password','$email','$fullname','$college','$gender','$dob');";
+        if(mysqli_query($con,$sql)){
+            $status = "Registration Success";
+            echo $status;
+        }else{
+            $status = "Resgistration Failed";
+            echo $status;
+        }
+    }
+    echo json_encode(array("response" => $status));
+    mysqli_close($con);
+
+?>
